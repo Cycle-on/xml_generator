c0459e7c864ba375e3d42b6a32809ebb733eb3a0
Danya-Mac <guskir3565@gmail.com>
2024-08-05 08:36:14 +0300

    change file with creator

---
+++ file_creator.py
@@ -0,0 +1,63 @@
+import datetime
+import os
+
+from pydantic import BaseModel
+
+from test_cards import cards
+import xml.etree.ElementTree as ET
+
+
+def __generate_xml_from_pydantic(root: ET.Element, model: dict, name='ukio'):
+    """
+    recursive subtree generator from dicts
+    adding different type subtrees to root
+    :param root:
+    :param model:
+    :param name:
+    :return:
+    """
+    sub_root = ET.SubElement(root, name)
+    for feature_name, feature_value in model.items():
+        if isinstance(feature_value, dict):  # if we have pydantic model
+            __generate_xml_from_pydantic(sub_root, feature_value, name=feature_name)
+            continue
+        elif isinstance(feature_value, datetime.datetime):
+            feature_value = feature_value.isoformat()
+        elif isinstance(feature_value, list):
+            sub_root_wrapper = ET.SubElement(sub_root, feature_name)
+            for value in feature_value:
+                __generate_xml_from_pydantic(sub_root_wrapper, value, name=feature_name[:-1])
+            continue
+
+        el = ET.SubElement(sub_root, feature_name)
+        el.text = str(feature_value)
+    return sub_root
+
+
+def create_file_from_model(model: BaseModel, filename: str = 'output') -> str:
+    """
+    function which create xml file from pydantic model
+    :param filename: string format
+    :param model: pydantic model
+    :return:  True -> file was saved successful
+              False -> some exceptions
+    """
+    try:
+        root_ = ET.Element("ukio")
+        sub_root = __generate_xml_from_pydantic(root_, model.dict())
+        tree = ET.ElementTree(sub_root)
+        tree.write(f"{filename}.xml")
+        return True
+    except Exception as ex:
+        with open(os.path.join("logs", "xml_creator", datetime.datetime.now().isoformat()), "w+") as f:
+            f.write(str(ex))
+        return False
+
+
+def main():
+    c1 = cards[0]
+    create_file_from_model(c1)
+
+
+if __name__ == '__main__':
+    main()
